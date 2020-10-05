c64f4edc07f54f0cad2e3adbda11306c19a036aa
Angel Aguado <angeagu@gmail.com>
2020-10-06 00:09:22 +0200

    Mas ejercicios

--- soluciones/estructurasdatos.py
+++ soluciones/estructurasdatos.py
@@ -1,6 +1,20 @@
+# Crear una funcion que tome como entrada dos listas del mismo tamaño y genere como
+# resultado una lista que es la suma de las listas.
+# def sumaMatrices(matriz1,matriz2):
+#     resultado = [[0 for x in range(len(matriz1))] for x in range(len(matriz1[0]))]
+#     for fila in range(len(matriz1)):
+#         for columna in range(len(matriz1[0])):
+#             resultado[fila][columna] = matriz1[fila][columna] + matriz2[fila][columna]
+#     return resultado
+#
+# m1 = [[1,2,4],[5,3,1],[6,3,9]]
+# m2 = [[2,4,5],[5,6,6],[9,8,7]]
+# print(sumaMatrices(m1, m2))
+
 
 # Crear una funcion que tome como entrada una matriz y calcule la matriz resultante de
 # calcular para cada posicion la media de su fila y columna.
-
-# Crear una funcion que tome como entrada dos listas del mismo tamaño y genere como
-# resultado una lista que es la suma de las listas.
+# matriz = [[2,4,5],[5,6,6],[9,8,7]]
+# for fila in range(len(matriz)):
+#     for columna in range(len(matriz[0])):
+#         print(matriz[fila][columna])
--- soluciones/funciones.py
+++ soluciones/funciones.py
@@ -1,15 +1,63 @@
 # Escribir una funcion que a partir del alto y ancho de un rectangulo, muestre por pantalla
 # Su area y su perimetro.
+# def rectangulo(alto, ancho):
+#     print("Area: " + str(alto * ancho))
+#     print("Perimetro: " + str(alto*2 + ancho*2))
+#
+# rectangulo(12,4)
 
 # Escribir una funcion que Verifique si una cadena de texto almacenada, es un DNI correcto o no.
 # Si lo es, se devolvera true y si no lo es, se devolvera false.
 # Los DNI tienen 8 dígitos y, a continuación una letra (no importa que sea mayúscula o minúscula).
+# def validaDNI(dni):
+#     valido = True
+#     cad = str(dni)
+#     if not cad[0:len(cad)-1].isdigit():
+#         valido = False
+#     else:
+#         if not cad[len(cad)-1].isalpha():
+#             valido = False
+#     return valido
+#
+# print(validaDNI("124A1242Z"))
+# print(validaDNI("71938103Z"))
+
 
 # Crear una funcion que tome como entrada dos cadenas y devuelva el numero de caracteres que tienen
-# en comun las dos cadenas.
+# # en comun las dos cadenas.
+# def caracteresComunes(cad1,cad2):
+#     comunes = 0
+#     for letra in cad1:
+#         if cad2.find(letra)!=-1:
+#             comunes += 1
+#     return comunes
+#
+# print(caracteresComunes("Valladolid","Dario"))
+
 
 # Modificar la funcion anterior para que en lugar del numero de caracteres en comun, devuelva los
 # caracteres que tienen en comun.
+# def caracteresComunes(cad1,cad2):
+#     comunes = '';
+#     for letra in cad1:
+#         if cad2.find(letra)!=-1 and comunes.find(letra)==-1:
+#             comunes += letra
+#     return comunes
+#
+# print(caracteresComunes("Valladolid","Dario"))
+
 
 # Crear una funcion que indique si una palabra es abedeceira. Una palabra es abecedeira si todas
 # sus letras estan ordenadas en orden alfabetico.
+# def abecedeira(palabra):
+#     palabra = palabra.lower()
+#     abecedeira = True
+#     indice = 0
+#     while(indice < len(palabra)):
+#         if indice < len(palabra)-1 and ord(palabra[indice]) > ord(palabra[indice+1]):
+#             abecedeira = False
+#         indice += 1
+#     return abecedeira
+#
+# print(abecedeira("Aguado"))
+# print(abecedeira("Bello"))
